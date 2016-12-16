1b42d77db3d3c004072933857a5b6e07e60ae5e6
Lucas Vieira <lucas.vieira@sirius.com.br>
2016-12-16 21:15:15 -0200

    Commit com upload de arquivo

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -6,16 +7,14 @@ from .forms import PostForm
 from .models import Post
 
 def posts_create(request): 
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
+
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
 
 		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
-	
-	else:
-		messages.success(request, "Not Successfully Created")
 	
 	context = {
 		"form": form,
@@ -36,20 +35,27 @@ def posts_detail(request, id=None):  #retrieve
 
 
 def posts_list(request): #list itens
-	
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all()
+	paginator = Paginator(queryset_list,6) # Show 25 contacts per page
+	page = request.GET.get('page')
+
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		queryset = paginator.page(1)
+	except EmptyPage:
+		queryset = paginator.page(paginator.num_pages)
 
 	context = {
-	 	"object_list": queryset,
+		"object_list": queryset,
 		"title": "List",
 	}
-
 
 	return render(request, "post_list.html",context)
 
 def posts_update(request, id=None):  #retrieve
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
--- src/trydjangoblog/settings.py
+++ src/trydjangoblog/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -121,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
